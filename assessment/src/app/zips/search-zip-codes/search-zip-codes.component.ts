1074af7a86030d01e4c91678c06224a271b57463
Gus Crawford <gus.crawford@tngoc.com>
2024-01-04 14:09:14 -0500

    my correction to the issue where the search creates a subscription

--- assessment/src/app/zips/search-zip-codes/search-zip-codes.component.ts
+++ assessment/src/app/zips/search-zip-codes/search-zip-codes.component.ts
@@ -1,24 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SearchZipsService, ZipCode } from '../search-zips.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search-zip-codes',
   templateUrl: './search-zip-codes.component.html',
   styleUrl: './search-zip-codes.component.scss'
 })
-export class SearchZipCodesComponent {
+export class SearchZipCodesComponent implements OnInit,OnDestroy {
   public results?: ZipCode[];
+  protected cleanUpOnDestroy = new Subscription();
+  protected searchFilter = {
+    expression: '',
+    state: ''
+  };
   constructor(public readonly zipSearch:SearchZipsService) { }
-  async search(subject:string) {
-    const [exp, state] = (subject??'').split(/\s*,\s*/g);
-    this.zipSearch.zips.subscribe(
-      zips=>{
-        this.results = this.zipSearch.filterResults(zips, exp, state);
-      },
-      err=>{
-        console.error(err);
-      }
+
+  ngOnInit(): void {
+    this.cleanUpOnDestroy.add(
+      this.zipSearch.zips.subscribe(
+        zips=>{
+          this.results = this.zipSearch.filterResults(zips, this.searchFilter.expression, this.searchFilter.state);
+        },
+        err=>{
+          console.error(err);
+        }
+      )
     );
+  }
+
+  async search(subject:string) {
+    const [expression, state] = (subject??'').split(/\s*,\s*/g);
+    this.searchFilter = {expression, state};
     this.zipSearch.refreshZipCodes();
+  }
+
+  ngOnDestroy(): void {
+    this.cleanUpOnDestroy.unsubscribe();
   }
 }
